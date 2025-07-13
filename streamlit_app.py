5a9eb2921bd0a5fadcba6c78eb179397087a8556
Miil-Kang <satisfe00@hanmail.net>
2025-07-14 01:08:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,8 @@ if uploaded_file:
     st.write("Preview of uploaded data:")
     st.dataframe(df.head())
 
-    # 모델 불러오기
-    model = joblib.load("model.pkl")  # 모델 경로가 정확한지 확인하세요
+    model = joblib.load("model.pkl")
 
-    # 예측 버튼
     if st.button("예측하기"):
         predictions = model.predict(df)
         df["예측 결과"] = ["입원" if p == 1 else "퇴원" for p in predictions]
